64d57d37c8b75df36cd0fe56c16e37b1533aefc7
Slow Beef <slowbeef@gmail.com>
2019-11-07 21:44:43 -0500

    Lots of refactoring!

--- mes_decrypt.py
+++ mes_decrypt.py
@@ -1,88 +1,93 @@
 #!/usr/bin/python
 
+#TODO: Genericize Nametags
+#TODO: Conditionals!
+
 import re
+import csv
 
 #outputType = 'lined'
 outputType = 'spreadsheet'
 
-filename = './OPEN_1.MES'
+mesName = 'OPEN_2'
+filename = mesName + '.MES'
+
+def writeJapaneseToTranslationFile(japaneseLines):
+    if outputType == 'lined':
+        for (nameTag, japanese, english) in japaneseLines:
+            print (nameTag + ': ' + line.decode('shift-jis')).encode('utf-8', 'ignore') + 'English: \n'
+    elif outputType == 'spreadsheet':
+        with open(mesName + '.CSV.1', 'w') as csvfile:
+            fieldnames = 'Nametag','Japanese','English'
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for (nameTag, japanese, english) in japaneseLines:
+                writer.writerow({'Nametag' : nameTag, 'Japanese' : japanese.decode('shift-jis').encode('utf-8', 'ignore'), 'English' : ''})
+
+puncutation = {
+    '\x0D' : '\x81\x41',
+    '\x0E' : '\x81\x64',
+    '\x0F' : '\x81\x42',
+    '\x10' : '\x81\x40',
+    '\x11' : '\x81\x49',
+    '\x12' : '\x81\x48',
+    '\x13' : '\\n',
+}
+
+# TODO : Don't hardcode these!
+# (but maybe it's good enough to just associate them with the MES file than parse how DotB actually does it)
+nameTags = {
+    '\x23' : 'Cole',
+    '\x24' : 'Cooger',
+    '\x25' : 'Officer Jack',
+}
+
 with open(filename, 'rb') as f:
     content = f.read()
 # get lines from bytes start marker BA 23-25 to end marker BA 26
 # 23 == cole, 24 == doc, 25 == jack(?)
 results = re.findall(br'(\xBA[\x23-\x25].*?)\xBA\x26', content)
+extractedLines = []
 if results:
-    if outputType == 'spreadsheet':
-        print "Nametag,Japanese,English"
-    for r in results:
-        # print repr(r)
-        # r = results[0]
-        sym = False
-        control = False
+    for result in results:
+        isPunctuation = False
+        isControl = False
         skip = False
         sub = ''
-        for c in r:
+        english = ''
+        nameTag = ''
+        for c in result:
             if c == '\xBA' and not skip:    # control byte
-                control = True
-            elif control and c == '\x23':   # dialog start cole
-                if outputType == 'lined':
-                    sub += '\x83\x52\x81\x5B\x83\x8B\x81\x46'
-                elif outputType == 'spreadsheet':
-                    sub += 'Cole,'
-                control = False
-            elif control and c == '\x24':   # dialog start doc
-                if outputType == 'lined':
-                    sub += '\x83\x4E\x81\x5B\x83\x4B\x81\x5B\x81\x46'
-                elif outputType == 'spreadsheet':
-                    sub += 'Cooger,'
-                control = False
-            elif control and c == '\x25':   # dialog start jack?
-                if outputType == 'lined':
-                    sub += '\x81\x5B\x81\x46'
-                elif outputType == 'spreadsheet':
-                    sub += 'Officer Jack,'
-                control = False
-            elif control and c == '\x26':   # dialog end
-                control = False
-            elif control and c == '\x28':   # symbol byte
-                sym = True
-                control = False
-            elif sym and c == '\x0D':       # comma
-                sub += '\x81\x41'
-                sym = False
-            elif sym and c == '\x0E':       # ..
-                sub += '\x81\x64'
-                sym = False
-            elif sym and c == '\x0F':       # period
-                sub += '\x81\x42'
-                sym = False
-            elif sym and c == '\x10':       # space
-                sub += '\x81\x40'
-                sym = False
-            elif sym and c == '\x11':       # !
-                sub += '\x81\x49'
-                sym = False
-            elif sym and c == '\x12':       # ?
-                sub += '\x81\x48'
-                sym = False
-            elif sym and c == '\x13':       # newline for two characted dialog in one box
-                sub += '\n'
-                sym = False
+                isControl = True
+            elif isControl and re.match('[\x23-\x25]', c):   # dialog start cole
+                if nameTag == '':
+                    nameTag = nameTags[c]
+                else:
+                    sub += nameTags[c] + ': '
+                isControl = False
+            elif isControl and c == '\x28':   # punctuation control byte
+                isPunctuation = True
+                isControl = False
+            elif isPunctuation and re.match('[\x0D-\x13]', c): # punctuation value
+                sub += puncutation[c]
+                isPunctuation = False
             elif skip:
                 sub += c
                 skip = False
             elif re.match('[\x81-\x83,\x88-\x9F,\xE0-\xEA]', c):    # kana, kanji and some symbols - skip next byte
                 sub += c
                 skip = True
-            elif sym or control:
-                print "ERROR sym/control byte skipped"
-                sym = False
-                control = False
+            elif isPunctuation or isControl:
+                print "ERROR isPunctuation/control byte skipped"
+                isPunctuation = False
+                isControl = False
             else:
                 # hiragana char, prefix 82 and shift by 74
+                print hex(ord(c))
                 sub += '\x82' + chr(ord(c)+114)
-        if outputType == 'lined':
-            sub += '\n'
-            print (sub.decode('shift-jis')).encode('utf-8', 'ignore') + 'English: \n'
-        elif outputType == 'spreadsheet':
-            print (sub.decode('shift-jis')).encode('utf-8', 'ignore') + ','
+        japanese = sub
+        extractedText = (nameTag, japanese, english)
+        extractedLines.append(extractedText)
+
+writeJapaneseToTranslationFile(extractedLines)
