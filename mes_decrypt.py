56bcdccf31f96012607b225aa870065f8455ce5d
Slow Beef <slowbeef@gmail.com>
2019-12-03 21:12:02 -0500

    Extracts all files from Disk 1 without errors. (Misses a few use cases, insertion seems off.)

--- mes_decrypt.py
+++ mes_decrypt.py
@@ -13,7 +13,7 @@ import argparse
 
 import nametags
 
-debugBytes = True  # Turn this on manually to export the bytes being read to standard out; useful if the script breaks
+debugBytes = False  # Turn this on manually to export the bytes being read to standard out; useful if the script breaks
 gotTranslation = False
 
 #outputType = 'lined'
@@ -31,6 +31,8 @@ parser = argparse.ArgumentParser(description='MES files to pass in')
 parser.add_argument("-m", default="MES_IN/000001", help="This is the name of the MES file with the directory it's in (and where to put stuff)")
 args = parser.parse_args()
 mesName = args.m
+
+print "Working on " + mesName
 
 filename = mesName + '.MES'
 transFile = mesName + '.ENG.CSV'
@@ -88,6 +90,9 @@ def encodeEnglish(line):
     english = english.replace('<C523280f>',b'\x00\x81\x40\xC5\x23\x28\x0F\x21')
     english = english.replace('<86A2>',b'\x00\x86\xA2\x21')
     english = english.replace('<A0A1>',b'\x00\xA0\xA1\x21')
+    english = english.replace('<LONEIF>',b'\x00\x81\x40\xBC\xA2\x21')
+    english = english.replace('<BA27>',b'\xBA\x27')
+
 
     #Cleanup
 #    english = english.replace(b'\x81\x40\xBA\x26', b'\xBA\x26')
@@ -167,18 +172,17 @@ encodedJapaneseLines = []
 # Nonstandard lines like the telephone ring/automated message are A8280f. This matches a bunch of other stuff so we're avoiding BB and D0 if they appear right afterwards.
 # This should come first because dialogue boxes will also match second/third lines in these sorts of
 # constructs, so the replace will modify one line in multi-line replacements.
-encodedJapaneseLines = encodedJapaneseLines + re.findall(br'\xA8\x28\x0F([^\xBB\xD0\x21].*?)(?:\x0C.)?(?:\x0D.)?(?:(?:\xBA\x26)|(?:\xA3\xFF\xFF)|(?:\xA3\xA4)|(?:\xC3\x23\x24)|(?:\xCD\x2A))', encodedMESbytes)
+encodedJapaneseLines = encodedJapaneseLines + re.findall(br'\xA8\x28\x0F([^\xBB\xD0\x21].*?)(?:\x0C.)?(?:\x0D.)?(?:(?:\xBA\x26)|(?:\xA3\xFF\xFF)|(?:\xA3\xA4)|(?:\xC3\x23\x24)|(?:\xCD\x2A)|(?:\xC6\x28))', encodedMESbytes)
 
-encodedJapaneseLines = encodedJapaneseLines + re.findall(br'[\xA8\xAA]\x28\x0E([^\xA6\xAC\xAF\xB0\xB4\xB6\xCD\xC9\xC5-\xC6\xD0(\xBC\xA2\x08)].*?)(?:\x0C.)?(?:(?:\xAB\xAA)|(?:\xBA\x26)|(?:\xA3?\xFF\xFF)|(?:\xA3\xA4)|(?:\xC3\x23\x24)|(?:\xCD\x2A))', encodedMESbytes)
+encodedJapaneseLines = encodedJapaneseLines + re.findall(br'[\xA8\xAA]\x28\x0E([^\xA6\xAC\xAD\xAF\xB0\xB4\xB6\xCD\xC9\xC5-\xC6\xCF\xD0(\xBC\xA2\x08)].*?)(?:\x0C.)?(?:(?:\xAB\xAA)|(?:\xBA\x26)|(?:\xA3?\xFF\xFF)|(?:\xA3\xA4)|(?:\xC3\x23\x24)|(?:\xCD\x2A)|(?:\xC6\x28)|(?:\xAC\x28))', encodedMESbytes)
+#encodedJapaneseLines = encodedJapaneseLines + re.findall(br'[\xA8\xAA]\x28\x0E([^\xA6\xAC\xAD\xAF\xB0\xB4\xB6\xC9\xC5-\xC6\xCF\xD0(\xBC\xA2\x08)].*?)(?:\x0C.)?(?:(?:\xAB\xAA)|(?:\xBA\x26)|(?:\xA3?\xFF\xFF)|(?:\xA3\xA4)|(?:\xC3\x23\x24)|(?:\xCD\x2A)|(?:\xC6\x28)|(?:\xAC\x28))', encodedMESbytes)
 
 # This regex matches standard dialog boxes, usually BA23-25...BA26. But as you can see, they can also end on A3A3, A4, C92242 or C32324. Note A4 can also appear as <ELSE> mid-dialog.
 # Note BA23-25 are nametags. They're technically not delimiters, but dialogue boxes can flow right after one another so BA26 may immediately be followed by BA23-25
-#encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA[\x23-\x25].*?)(?:\x0c.)?(?:\x19\x90)?(?:\x0c.)?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xA4\xBA[\x23-25])|(?:\xC9\x22\x42)|(?:\xC3\x23\x24))', encodedMESbytes)
-#encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA[\x23-\x25].*?)(?:\x0c.)?(?:\x19\x90)?(?:\x0c.)?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xC9\x22\x42)|(?:\xC3\x23\x24))', encodedMESbytes)
-encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA[\x23-\x25].*?)(?:\x0c.)?(?:\x19\x90)?(?:\x0c.)?(?:\x0d\xf6)?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xC3[\x23-\x24]\x24)|(?:\xC1\x23)|(?:\xCC\x28\x14))', encodedMESbytes)
+encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA[\x23-\x25].*?)(?:\xBC\xA2)?(?:[\x0c\x0d].)?(?:\x19\x90)?(?:\x0c.)?(?:\x0d[\xf6-\xf8])?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xC3[\x23-\x24]\x24)|(?:\xC1\x23)|(?:\xCC\x28\x14)|(?:\xD0\x73)|(?:\xC6\x28))', encodedMESbytes)
 
 # As of 000039.MES, instead of nametag macros (BA23) it'll use BA2804-0C for nametag macros.
-encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA\x28[\x04-\x0C].*?)(?:\x0c.)?(?:\xD0\x24)?(?:\x19\x90)?(?:\x0d\xf6)?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xC3[\x23-\x24]\x24)|(?:\xC1\x23)|(?:\xCC\x28\x14))', encodedMESbytes)
+encodedJapaneseLines = encodedJapaneseLines + re.findall(br'(\xBA\x28[\x04-\x0C].*?)(?:\x0c.)?(?:\xD0\x24)?(?:\x19\x90)?(?:\x0d[\xf6-\xf8])?(?:(?:\xBA\x26)|(?:\xA3\xA3)|(?:\xC3[\x23-\x24]\x24)|(?:\xC4\x23\x23)|(?:\xC1[\x23\x24])|(?:\xCC\x28\x14)|(?:\xD0\x73)|(?:\xD0\x23))', encodedMESbytes)
 
 # Nonstandard lines - usually with no nametag - start with A4AA280E...AC
 #encodedJapaneseLines = encodedJapaneseLines + re.findall(br'\xAA\x28\x0E(.*?)(?:(?:\xBA\x26)|(?:\xAC)|(?:\xFF\xFF))', encodedMESbytes)
@@ -219,6 +223,7 @@ if encodedJapaneseLines:
 
         result = re.sub(br'\xBC\xA2\x0C', b'\xBC', result) # Flag tests are multibyte and annoying to deal with so let's pare it down
         result = re.sub(br'\xBC\xA2\x08', b'\xB8', result) # Is this another flag test? Might be a setter? Not sure diff between 08 and 0C
+        result = re.sub(br'\xBC\xA2([^\x0C\x08])', b'\xBF\\1', result) # BCA2 by itself? It happens (000044)
 
         result = re.sub(br'\xA8\x28\x05', b'\xAB', result) # TODO: Remove this, just temp to debug this weird part
         result = re.sub(br'\xCF\x24\x23', b'\xCF', result) # C9-CF2423 defines an image which can appear mid dialog.
@@ -268,6 +273,8 @@ if encodedJapaneseLines:
             elif c == '\xB8':
                 sub += '<SET '
                 isFlag = True
+            elif c == '\xBF':
+                sub += '<LONEIF>'
             elif c == '\xA8':
                 sub += '<A828OF>'
             elif c == '\xA2' and isConditional:
@@ -309,6 +316,9 @@ if encodedJapaneseLines:
                     nameTag = nameTags[c]
                 else:
                     sub += nameTags[c] + ': '
+                isControl = False
+            elif isControl and c == '\x27':
+                sub += '<BA27>'
                 isControl = False
             elif isControl and c == '\x28':   # punctuation control byte
                 isPunctuation = True
